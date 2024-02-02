f6e52b222817fbe6dbad36b8f993687896958c56
josh-susinth <joshsusinth@gmail.com>
2024-02-02 23:53:09 +0530

    removed login

--- src/route/signUp.js
+++ src/route/signUp.js
@@ -16,28 +16,8 @@ const signUp = async (req, res) => {
     }
 }
 
-const logIn = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const { getUser } = USER;
-        const user = await getUser(email);
-        if (!user) {
-            console.log("invalid User")
-            res.send("email error")
-        }
-        if (user.password !== password) {
-            console.log("Invalid Credentials");
-            res.send("pwd error")
-        }
-        console.log(`${user.name} logged in`);
-        res.send(200);
-    }
-    catch (e) {
-        console.log(e);
-    }
-}
+
 
 router.route("/signup").post(signUp);
-router.route("/login").post(logIn);
 
 module.exports = router;
